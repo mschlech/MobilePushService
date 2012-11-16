cc496c891bea98782e95c9e8bad1ec8761ec4f37
marcus <marcus.schlechter@yahoo.de>
2012-11-16 17:11:48 +0100

    minor change

--- src/main/scala/com/entscheidungsbaum/Pushnotifier.scala
+++ src/main/scala/com/entscheidungsbaum/Pushnotifier.scala
@@ -1,25 +1,26 @@
 package com.entscheidungsbaum
 
-import akka.actor.Actor
 import akka.actor.ActorRef
 import akka.actor.actorRef2Scala
-import akka.camel.CamelMessage
-import akka.camel.Consumer
+import akka.actor.ActorSystem
+import akka.actor.Props
+import com.sun.org.apache.bcel.internal.generic.PUSH
 
-case class PushType(s: Any)
+class PushnotifierAppWith //extends Bootable {
+{
+  val pushServiceActor = ActorSystem("pushnotifier")
 
-class Pushnotifier(producer: ActorRef) extends Actor with Consumer {
-  def endpointUri = "jetty:http://0.0.0.0:1113/apn"
+  def startup = {
+    //    pushServiceActor.actorOf(Props(new InternalActor), "internalActor")
 
-  def receive = {
+ //   pushServiceActor.actorOf(Props(new ApplePushConsumer("activemq:queue:activemqApple", internalActorRef)), "applepushconsumer") ! Start
 
-    //  case pushRequest: CamelMessage => processDispatcher(PushType(pushRequest.getHeader("apnType")))
-    case _ => println("did not understand the request")
+  //  pushServiceActor.actorOf(Props(new AndroidPushConsumer("activemq:queue:activemqAndroid")), "androidpushconsumer") ! Start
+
+    //    pushServiceActor.actorOf(Props(new QProducer("activemq:queue:activemqApple")))
   }
 
-  def processDispatcher(dispatchCamelRequest: PushType) = dispatchCamelRequest match {
-    case s: PushType => sender ! s
-    case _ => println("nothing")
+  def shutdown = {
+    pushServiceActor.shutdown
   }
-
 }
