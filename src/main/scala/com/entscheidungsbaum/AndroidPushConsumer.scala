7c4aac3e851aea0a399fc78859bae3daaac95d32
marcus <marcus.schlechter@yahoo.de>
2012-11-16 17:10:18 +0100

    minor change

--- src/main/scala/com/entscheidungsbaum/AndroidPushConsumer.scala
+++ src/main/scala/com/entscheidungsbaum/AndroidPushConsumer.scala
@@ -4,16 +4,18 @@ import akka.actor.actorRef2Scala
 import akka.camel.Ack
 import akka.camel.CamelMessage
 import akka.camel.Consumer
+import akka.camel.Producer
+import akka.actor.Actor
 
-class AndroidPushConsumer(androidQueue: String) extends Consumer { 
+class AndroidPushConsumer(androidQueue: String) extends Actor with Producer { 
   println(" AndroidPushConsumer =[" + androidQueue + "]" )
-  def endpointUri = androidQueue
+  def endpointUri = "file:///Users/marcus/tmp"
 
 //  override def activationTimeout = 10 seconds
 //  override def replyTimeout = 30 seconds
 //  override def autoack = false
 
-  def receive = {
+  override def transformOutgoingMessage(msg:Any) = msg match {
 
     case msg: CamelMessage => {
 
