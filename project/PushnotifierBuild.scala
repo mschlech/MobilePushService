3af0f247d189dbe1d1df0641e6cc45f5c60c97c2
marcus <marcus.schlechter@yahoo.de>
2012-11-16 17:13:15 +0100

    minor change

--- project/PushnotifierBuild.scala
+++ project/PushnotifierBuild.scala
@@ -17,9 +17,9 @@ object PushnotifierBuild extends Build {
         "org.scala-lang" % "scala-reflect" % "2.10.0-RC1",
         "com.typesafe.akka" % "akka-camel_2.10.0-RC1" % "2.1.0-RC1",
         "com.typesafe.akka" % "akka-testkit_2.10.0-RC1" % "2.1.0-RC1",
-        "org.apache.activemq" % "activemq-core" % "5.4.1",
-        "org.apache.activemq" % "activemq-camel" % "5.4.1",
+        "org.apache.activemq" % "activemq-core" % "5.7.0",
+        "org.apache.activemq" % "activemq-camel" % "5.7.0",
         "org.apache.camel" % "camel-jetty" % "2.9.3" % "compile",
-        "org.apache.camel" % "camel-core" % "2.10.0" exclude ("org.slf4j", "slf4j-api"),
+        "org.apache.camel" % "camel-core" % "2.10.0"  exclude ("org.slf4j", "slf4j-api"),
         "org.scalatest" % "scalatest_2.10.0-RC1" % "2.0.M4-2.10.0-RC1-B1")))
 }
